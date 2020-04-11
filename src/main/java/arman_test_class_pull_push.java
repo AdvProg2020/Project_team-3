4b9b6fc19f67f41906e8ad0c224d62a3f6d278de
Arman5592 <soleimaniarman98@gmail.com>
2020-04-11 22:34:20 +0430

    added testclass

---
+++ src/main/java/arman_test_class_pull_push.java
@@ -0,0 +1,11 @@
+public class arman_test_class_pull_push {
+    private int myNumber;
+
+    public void setMyNumber(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    public int getMyNumber() {
+        return myNumber;
+    }
+}
