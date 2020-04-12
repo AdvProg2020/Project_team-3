eb8c6954bb608eb008f7b29b51e88525eb3018bb
Alireza Eiji <AlirezaEiji151379@gmail.com>
2020-04-12 23:35:44 +0430

    constructor added to Rating

--- src/main/java/Rating.java
+++ src/main/java/Rating.java
@@ -3,8 +3,26 @@ public class Rating {
     private String username;
     private String itemId;
 
+
+    //getters
     public int getScore() {
         return score;
     }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    //constructor
+    public Rating(int score, String username, String itemId) {
+        this.score = score;
+        this.username = username;
+        this.itemId = itemId;
+    }
+
 
 }
