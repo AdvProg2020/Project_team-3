4fb02eb28fdf5dc7c7a9893adf45572a1bb605ac
Alireza Eiji <AlirezaEiji151379@gmail.com>
2020-04-13 00:33:52 +0430

    User set as abstract

--- src/main/java/AddAccountRequest.java
+++ src/main/java/AddAccountRequest.java
@@ -1,3 +1,16 @@
-public class AddAccountRequest {
+
+public class AddAccountRequest extends  Request{
+    private Seller seller;
+    public AddAccountRequest(String requestId , Seller seller) {
+        super(requestId);
+        this.seller=seller;
+        Request.addRequest(this);
+        String news="the new seller with user Name"+seller.getUsername()+"wants to create account in your System";
+        this.setMassage(news);
+    }
+    
+    public Seller getSeller() {
+        return seller;
+    }
 
 }
