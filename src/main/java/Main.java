be64f8d4222b81a81e62bd4c9e3302a8aa72f814
Alireza Eiji <AlirezaEiji151379@gmail.com>
2020-04-12 13:00:48 +0430

    test1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        System.out.println("Alireza Eiji is the man of focus!!!!");
     }
 }
