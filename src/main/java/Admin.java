1fbfcb35f2953945fd2fd83e1307db6a7e24bdf3
amirreza <amirreza2001@gmail.com>
2020-04-12 22:16:30 +0430

    admin update

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,5 +1,22 @@
 public class Admin extends User {
-    public Admin(String username,String password,String name,String lastName,String email,String type,String number){
-        super( username, password, name, lastName, email, type, number);
+
+    public Admin(String username, String password, String name, String lastName, String email, String type, String number) {
+        super(username, password, name, lastName, email, type, number);
     }
+
+    public void addAdminAccount(String username, String password, String name, String lastName, String email, String type, String number) {
+
+    }
+
+    public void deleteUser(String username) {
+
+    }
+
+    public String showCategory(String categoryName) {
+           return "salam";
+    }
+
+    // public void addCategory(  ta vaghti category neveshte beshe  ) {
+    //   }
+
 }
