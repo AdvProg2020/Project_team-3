39eddbf967b6b0549386fcc27bfc25b0b47d649d
Arman5592 <soleimaniarman98@gmail.com>
2020-04-13 23:24:51 +0430

    menus updated

---
+++ src/main/java/ItemMenu.java
@@ -0,0 +1,24 @@
+public class ItemMenu extends Menu {
+    private static ItemMenu itemMenu;
+    private int optionCount = 4;
+    private ItemMenu(){ }
+
+    public static ItemMenu getInstance(){
+        if(itemMenu==null)
+            itemMenu = new ItemMenu();
+        return itemMenu;
+    }
+    @Override
+    public void show(){
+
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
+    @Override
+    public void execute(String command){
+
+    }
+}
