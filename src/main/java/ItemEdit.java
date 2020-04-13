8792b6d5a4ec5f6635fef14a0a49a07fc0799d3f
Alireza Eiji <AlirezaEiji151379@gmail.com>
2020-04-13 17:39:59 +0430

    update the Sale Edit and Item Edit Classes!

--- src/main/java/ItemEdit.java
+++ src/main/java/ItemEdit.java
@@ -1,12 +1,24 @@
 public class ItemEdit extends Request {
     private Item Item;
+    private String changedField;
+    private String newFieldValue;
     ///overloaded constructor
-    public ItemEdit(String requestId, Item Item) {
+    public ItemEdit(String requestId, Item Item , String changedField , String newFieldValue) {
         super(requestId);
         this.Item = Item;
+        this.changedField=changedField;
+        this.newFieldValue=newFieldValue;
     }
 
     public Item getNewItem() {
         return Item;
     }
+    public String getChangedField(){
+        return this.changedField;
+    }
+
+    public String getNewFieldValue() {
+        return newFieldValue;
+    }
+
 }
--- src/main/java/SaleEdit.java
+++ src/main/java/SaleEdit.java
@@ -1,13 +1,24 @@
 public class SaleEdit extends  Request{
     private Sale sale;
+    private String changedFiled;
+    private String newFieldValue;
     /////overloaded Constructor
-    public SaleEdit(String requestId, Sale sale) {
+    public SaleEdit(String requestId, Sale sale , String changedFiled , String newFieldValue) {
         super(requestId);
         this.sale = sale;
+        this.changedFiled=changedFiled;
+        this.newFieldValue=newFieldValue;
     }
 
     public Sale getSale() {
         return sale;
     }
 
+    public String getNewFieldValue() {
+        return newFieldValue;
+    }
+
+    public String getChangedFiled() {
+        return changedFiled;
+    }
 }
