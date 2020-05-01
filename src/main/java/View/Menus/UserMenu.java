7ab8f35011722af81359bbc5ebd53a322f6fb818
Arman5592 <soleimaniarman98@gmail.com>
2020-05-01 22:35:01 +0430

    added ViewPersonalInfo class

--- src/main/java/View/Menus/UserMenu.java
+++ src/main/java/View/Menus/UserMenu.java
@@ -1,6 +1,15 @@
 package View.Menus;
 
+import Control.Controller;
+import Control.UserController;
+
 public abstract class UserMenu extends Menu {
+
+    public void viewPersonalInfo(){
+        System.out.println(UserController.getInstance().viewPersonalInfo(Controller.getInstance().getCurrentOnlineUser().getUsername()));
+        ViewPersonalInfo.getInstance().show();
+    }
+
     public void editPersonalInfo(){
 
     }
---
+++ src/main/java/View/Menus/ViewPersonalInfo.java
@@ -0,0 +1,50 @@
+package View.Menus;
+
+import Control.UserController;
+import Model.Users.Admin;
+import Model.Users.Seller;
+
+public class ViewPersonalInfo extends UserMenu {
+    private static ViewPersonalInfo viewPersonalInfo ;
+    private int optionCount = 4;
+    private ViewPersonalInfo(){ }
+
+    public static ViewPersonalInfo getInstance(){
+        if(viewPersonalInfo==null)
+            viewPersonalInfo = new ViewPersonalInfo();
+        return viewPersonalInfo;
+    }
+    @Override
+    public void help() {
+        System.out.println("Press E to edit personal info or X to go back. ");
+    }
+
+    @Override
+    public void show() {
+        help();
+        String command = View.read.nextLine();
+        execute(command);
+    }
+
+    @Override
+    public void execute(String command) {
+        if(command.equals("E")){
+
+        }else if(command.equals("X")){
+            if(UserController.getInstance().getCurrentOnlineUser() == null){
+                LoginRegisterMenu.getInstance().show();
+            }
+            if(UserController.getInstance().getCurrentOnlineUser() instanceof Admin){
+                AdminMenu.getInstance().show();
+            }else if(UserController.getInstance().getCurrentOnlineUser() instanceof Seller){
+                SellerMenu.getInstance().show();
+            }else{
+                BuyerMenu.getInstance().show();
+            }
+        }else{
+            System.out.println(View.ANSI_RED+"Invalid command."+View.ANSI_RESET);
+            show();
+        }
+    }
+
+}
