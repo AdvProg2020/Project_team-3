8d64d4a560798423b2dafeaca2c7a4b4fde13a0e
Arman5592 <soleimaniarman98@gmail.com>
2020-06-06 02:32:05 +0430

    hanoz ye buge maven has

---
+++ src/main/java/Controller/SceneSwitcher.java
@@ -0,0 +1,39 @@
+package Controller;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+
+public class SceneSwitcher {
+    private static SceneSwitcher sceneSwitcher;
+    private HashMap<String, Scene> allScenes = new HashMap<>();
+    private Stage stage;
+    private SceneSwitcher(){
+
+    }
+    public static SceneSwitcher getInstance(){
+        if(sceneSwitcher==null){
+            sceneSwitcher = new SceneSwitcher();
+        }
+        return sceneSwitcher;
+    }
+
+    public void addScene(String name,Scene scene){
+        allScenes.put(name,scene);
+    }
+
+    public void setStage(Stage passedStage){
+        this.stage = passedStage;
+    }
+
+    public void closeWindow(){
+        stage.close();
+    }
+
+    public void setSceneTo(String name){
+        stage.setScene(allScenes.get(name));
+    }
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 import Controller.Database;
-import Controller.ItemAndCategoryController;
-import View.Menus.ShopAndDiscountMenu.ShopMenu;
+import Controller.SceneSwitcher;
 import View.Menus.View;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+
+import java.io.IOException;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -17,11 +20,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        AnchorPane anchorPane = new AnchorPane();
+        SceneSwitcher.getInstance().setStage(stage);
         stage.setTitle("paniz");
-        stage.setScene(new Scene(anchorPane,400,400));
+        initializeScreens();
+        SceneSwitcher.getInstance().setSceneTo("FelanMenu");
         stage.show();
     }
+
+    public void initializeScreens() throws IOException {
+        //screen haro az fxml mikhone pass mide SceneSwitcher
+        Parent parent;
+        //parent = FXMLLoader.load(getClass().getResource("main/java/View/Menus/Markups/FelanMenu.fxml"));
+        parent = FXMLLoader.load(getClass().getResource("fxml/asd.fxml")); //<= kar nemikone kiri!
+        SceneSwitcher.getInstance().addScene("FelanMenu",new Scene(parent,400,600));
+    }
+
 }
 
 //edit category : add attribute [attribute e space dar]
