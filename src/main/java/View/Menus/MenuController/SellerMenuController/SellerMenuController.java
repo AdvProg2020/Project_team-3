4e0c03a440fcd146379027063e7066a6394784ed
Arman5592 <soleimaniarman98@gmail.com>
2020-06-11 21:44:20 +0430

    payeye sellermenu zade shod

---
+++ src/main/java/View/Menus/MenuController/SellerMenuController/SellerMenuController.java
@@ -0,0 +1,38 @@
+package View.Menus.MenuController.SellerMenuController;
+
+import Controller.SceneSwitcher;
+import Controller.UserController;
+import javafx.fxml.FXML;
+
+public class SellerMenuController {
+
+    @FXML
+    private void back(){
+        SceneSwitcher.getInstance().setSceneTo("MainMenu");
+    }
+    @FXML
+    private void logout(){
+        UserController.getInstance().logout();
+        SceneSwitcher.getInstance().setSceneTo("MainMenu");
+    }
+    @FXML
+    private void manageProducts(){
+        SceneSwitcher.getInstance().setSceneTo("SellerManageProductsMenu");
+    }
+    @FXML
+    private void addProduct(){
+        SceneSwitcher.getInstance().setSceneTo("SellerAddProductMenu");
+    }
+    @FXML
+    private void viewSalesHistory(){
+        SceneSwitcher.getInstance().setSceneTo("SellerSalesHistory");
+    }
+    @FXML
+    private void viewShop(){
+        SceneSwitcher.getInstance().setSceneTo("ShopMenu");
+    }
+    @FXML
+    private void viewDiscounts(){
+        SceneSwitcher.getInstance().setSceneTo("DiscountsMenu");
+    }
+}
