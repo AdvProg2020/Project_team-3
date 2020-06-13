b6869592dd0ac8e5bee898c69c9fd397ac12ddb9
Arman5592 <soleimaniarman98@gmail.com>
2020-06-14 01:19:45 +0430

    shop basis dpne

--- src/main/java/View/Menus/MenuController/ShopMenuController.java
+++ src/main/java/View/Menus/MenuController/ShopMenuController.java
@@ -1,14 +1,29 @@
 package View.Menus.MenuController;
 
+import Controller.ItemAndCategoryController;
+import Controller.SortAndFilterController;
 import Controller.UserController;
+import Model.Item;
+import View.Menus.ItemMenu;
 import View.Menus.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class ShopMenuController {
 
-
+    private String categoryName = "Main";
+    private ArrayList<String> itemsToString = new ArrayList<>();
+    private ArrayList<String> itemsID = new ArrayList<>();
+    private ArrayList<VBox> itemsVBox = new ArrayList<>();
 
     public void logout(ActionEvent actionEvent) {
         UserController.getInstance().logout();
@@ -30,10 +45,57 @@ public class ShopMenuController {
 
     @FXML
     private void initialize(){
+        initLists();
         //bayad item haye shop ro bedast biarim va chizayi mesle filter sort va category ke mitonan
         //arrayliste item haro dastkari konan ro poshesh bedim , inja miaim item haro bargozari mikonim
         //har item mishe 1 Vbox 3 ghesmate , 1 aks + 1 gheymat (agge off bashe strikethrough + gheymat jadid) + 1 esm
         //click rooye vbox mibare maro be itemMenu on item
         //catgory o filter o sort o in kosshera ro badan mizanim
+
+
     }
+
+    private void initLists(){
+        categoryName = "Main";
+        itemsToString = SortAndFilterController.getInstance().show(categoryName);
+        for(String string : itemsToString){
+            itemsID.add(string.substring(4,9));
+        }
+        int column=0,row=0;
+        for(String itemID : itemsID){
+            gridPane.add(createAndAddItem(itemID),column,row);
+            if(column==2){
+                column = 0;
+                row++;
+            }
+            column++;
+        }
+
+    }
+
+    private VBox createAndAddItem(String itemID){
+        Item item = ItemAndCategoryController.getInstance().getItemById(itemID);
+        VBox itemBox = new VBox();
+
+        itemBox.setOnMouseClicked(event -> {
+            ItemMenuController.setItemID(itemID);
+            SceneSwitcher.getInstance().setSceneTo("ItemMenu");
+        });
+        itemBox.setPrefSize(230,345);
+
+        ImageView imageView = new ImageView(new Image(new File("src/main/resources/Images/ItemImages/"+item.getImageName()).toURI().toString(),230,230,false,false));
+
+        Label name = new Label(item.getName());
+        Label price = new Label(Double.toString(item.getPrice()));
+
+        itemBox.getChildren().add(imageView);
+        itemBox.getChildren().add(name);
+        itemBox.getChildren().add(price);
+
+        itemsVBox.add(itemBox);
+        return itemBox;
+    }
+
+
+
 }
