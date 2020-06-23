10c0d6d68d90f29b096619e2aae78d6aaaa687d9
Arman5592 <soleimaniarman98@gmail.com>
2020-06-23 23:41:03 +0430

    spain but the s is silent

--- src/main/java/View/Menus/MenuController/SalesMenuController.java
+++ src/main/java/View/Menus/MenuController/SalesMenuController.java
@@ -1,11 +1,16 @@
 package View.Menus.MenuController;
 
+import Controller.SortAndFilterController;
 import Controller.UserController;
 import View.Menus.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class SalesMenuController {
+
+
+    private int pageNumber=1;
+
 
     public void logout(ActionEvent actionEvent) {
         UserController.getInstance().logout();
@@ -25,6 +30,36 @@ public class SalesMenuController {
 
     @FXML
     private void initialize(){
+
+    }
+
+    private void initLists(){
+
+    }
+
+
+    private boolean isAValidPage(int num){
+        return false;
+    }
+
+
+    @FXML
+    private void increasePage(){
+        if(isAValidPage(pageNumber+1)){
+            //pageNum.setText((pageNumber + 1) +"/"+ (SortAndFilterController.getInstance().show(categoryName).size() / 24 + 1));
+            pageNumber++;
+            initLists();
+        }
+
+    }
+
+    @FXML
+    private void decreasePage(){
+        if(isAValidPage(pageNumber-1)){
+            //pageNum.setText((pageNumber - 1) +"/"+ (SortAndFilterController.getInstance().show(categoryName).size() / 24 + 1));
+            pageNumber--;
+            initLists();
+        }
 
     }
 }
--- src/main/java/View/Menus/MusicManager.java
+++ src/main/java/View/Menus/MusicManager.java
@@ -1,6 +1,10 @@
 package View.Menus;
 
-public class MusicManager {
+public class MusicManager extends Thread{
+    @Override
+    public void run() {
+
+    }
     //in kire khar baraye pakhshe musice pas zamine ba thread hast
     //class e joda baes mishe garbage collector e kossher nayad eine alleh ahang ro ghat kone
 
