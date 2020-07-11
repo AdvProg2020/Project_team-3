d8238a23bf8545784f57392bc3192d954a4361fb
Arman5592 <soleimaniarman98@gmail.com>
2020-07-11 21:33:34 +0430

    asd

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module Project {
     requires com.google.gson;
     requires javafx.media;
     requires kotlin.stdlib;
-    requires javax.servlet;
+    requires javax.servlet.api;
     opens Project to javafx.fxml;
     opens Project.Model.Users to com.google.gson;
     opens Project.Model.Logs to com.google.gson;
