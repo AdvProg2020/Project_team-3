f4f51d9e26a7060e720db2d91632963ae1ce0dd7
Alireza <alirezaeiji151379@gmail.com>
2020-07-12 21:36:29 +0430

    server added!!!

--- src/main/java/Project/Controller/Server.java
+++ src/main/java/Project/Controller/Server.java
@@ -1,5 +1,54 @@
 package Project.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Server {
    ////inja bayad vasl beshe v payam begire badesh befreste bara proccess ke on payam ro anjam bede v javab ro befreste bara server v srrvera befreste client
+
+    private static Server server;
+
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    private Server(){}
+
+    public static Server getInstance(){
+        if(server==null) server=new Server();
+        return server;
+    }
+
+    public void setRequest(HttpServletRequest req){
+        request=req;
+    }
+
+    public  void setResponse(HttpServletResponse resp){
+        response=resp;
+    }
+
+    public void sendMessageToClient(String message){
+        try {
+            response.getOutputStream().println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String getClientMessage(){
+        BufferedReader bufferedReader= null;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            StringBuilder responseString = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = bufferedReader.readLine()) != null) {
+                responseString.append(responseLine.trim());
+            }
+            return  responseString.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  null;
+    }
 }
--- src/main/java/Project/Controller/Servlet.java
+++ src/main/java/Project/Controller/Servlet.java
@@ -10,32 +10,35 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 
 @WebServlet
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestURL = req.getRequestURI();
-        String username = requestURL.substring("/Project_team_3_war/Users/".length());
-        User user = UserController.getInstance().getUserByUsername(username);
-        resp.getOutputStream().println(new GsonBuilder().setPrettyPrinting().create().toJson(user));
+        Server.getInstance().setRequest(req);
+        Server.getInstance().setResponse(resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doPost(req, resp);
+        Server.getInstance().setRequest(req);
+        Server.getInstance().setResponse(resp);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doPut(req, resp);
+        Server.getInstance().setRequest(req);
+        Server.getInstance().setResponse(resp);
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doDelete(req, resp);
+        Server.getInstance().setRequest(req);
+        Server.getInstance().setResponse(resp);
     }
 }
