cf6d808eb15586d5df1dfe6f2d01776b318f4760
Arman5592 <soleimaniarman98@gmail.com>
2020-07-22 02:15:22 +0430

    huge elephant cock

--- src/main/java/Project/Client/Menus/MenuController/AllAuctionsMenu.java
+++ src/main/java/Project/Client/Menus/MenuController/AllAuctionsMenu.java
@@ -1,13 +1,16 @@
 package Project.Client.Menus.MenuController;
 
+import Project.Client.CLI.View;
 import Project.Client.MakeRequest;
 import Project.Client.Menus.MusicManager;
 import Project.Client.Menus.SceneSwitcher;
 import Project.Client.Model.Auction;
+import Project.Client.Model.SortAndFilter;
 import Project.Client.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -15,18 +18,24 @@ import java.util.ArrayList;
 public class AllAuctionsMenu {
 
     @FXML private ListView listView;
+    @FXML private AnchorPane pane;
+    private ArrayList<Auction> allAuctions;
 
     public void back(ActionEvent actionEvent)
     {
         MusicManager.getInstance().playSound("Button");
-        SceneSwitcher.getInstance().back();
+        SceneSwitcher.getInstance().setSceneTo("MainMenu");
     }
 
     @FXML private void initialize(){
         //miad list ro update mikone
         //on click : berim auction menu
+        SortAndFilter.getInstance().reset();
+        MakeRequest.resetFilter();
+        View.setFonts(pane);
+        listView.getItems().clear();
         String str = MakeRequest.getAllAuctions();
-        ArrayList<Auction> allAuctions = ObjectMapper.jsonToAuction(str);
+        allAuctions = ObjectMapper.jsonToAuction(str);
         for(Auction auction:allAuctions){
             listView.getItems().add(auction.getId()+"   for item "+auction.getItemID()+"  current bid:"+auction.getHighestBid()+"    ends:"+auction.getEndTime());
         }
@@ -34,6 +43,23 @@ public class AllAuctionsMenu {
     }
 
     @FXML private void select(){
-        //berim
+        int index=listView.getSelectionModel().getSelectedIndex();
+        if(index==-1)
+            return;
+        String id=listView.getItems().get(index).toString();
+        System.out.println(id);
+        id = id.substring(0,5);
+        listView.getSelectionModel().clearSelection();
+        AuctionMenu.setAuction(getSelected(id));
+        SceneSwitcher.getInstance().setSceneTo("AuctionMenu");
+    }
+
+    private Auction getSelected(String id){
+        for(Auction auction:allAuctions){
+            if(auction.getId().equals(id)){
+                return auction;
+            }
+        }
+        return null;
     }
 }
--- src/main/java/Project/Client/Menus/MenuController/AuctionMenu.java
+++ src/main/java/Project/Client/Menus/MenuController/AuctionMenu.java
@@ -1,13 +1,25 @@
 package Project.Client.Menus.MenuController;
 
+import Project.Client.CLI.ItemMenu;
+import Project.Client.Menus.MusicManager;
+import Project.Client.Menus.SceneSwitcher;
 import Project.Client.Model.Auction;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class AuctionMenu {
-    private Auction auction;
+    private static Auction auction;
 
-    protected void setAuction(Auction auction){
-        this.auction = auction;
+    @FXML private AnchorPane pane;
+
+    protected static void setAuction(Auction newAuction){
+        auction = newAuction;
+    }
+
+    @FXML  private void back() {
+        MusicManager.getInstance().playSound("Button");
+        SceneSwitcher.getInstance().setSceneTo("AllAuctionsMenu");
     }
 
     @FXML private void initialize(){
@@ -16,6 +28,20 @@ public class AuctionMenu {
         //ghabeliat chat beine hamme (login shode bashe!)
 
         //ghabeliat bid baraye buyer ha
+    }
+
+    private void initializeChat(){
+
+    }
+
+    @FXML private void sendChatMessage(){
+
+    }
+
+    @FXML private void learnMore(){
+        ItemMenuController.setItemID(auction.getItemID());
+        SceneSwitcher.getInstance().saveScene("AuctionMenu");
+        SceneSwitcher.getInstance().setSceneTo("ItemMenu");
     }
 
 }
